16a60ae2252a1725fbc8a8379ebf4eaec9b22482
nicolelst <nicolelim2608@gmail.com>
2023-10-12 00:39:09 +0800

    fix(rpi): update turning corrections

--- rpi/mdp-rpi/rpi_config.py
+++ rpi/mdp-rpi/rpi_config.py
@@ -13,22 +13,24 @@ BT_BUFFER_SIZE = 2048
 STM_BAUDRATE = 115200
 STM_ACK_MSG = "A"
 STM_COMMAND_FORMAT = '^[SLR][FB][0-9]{3}$'
+
+# adjust commands for turns to correct turning radius to 30cm, as expected by PC-algo
 STM_COMMAND_ADJUSTMENT_MAP = {
-    # 90 degree turns
-    "RF090": ["SF007", "RF090", "SB007"],
+    # 90 degree turns: manually calibrated
+    "RF090": ["SF007", "RF090", "SB008"],
     "LF090": ["SF007", "LF090", "SB011"],
     "RB090": ["SF009", "RB090", "SB009"],
-    "LB090": ["SF010", "LB090", "SB008"],
-    # 180 degree turns
-    "RF180": ["SF007", "RF180", "SB007"],
+    "LB090": ["SF009", "LB090", "SB006"],
+    # 180 degree turns: manually calibrated
+    "RF180": ["SF008", "RF180", "SB008"],
     "LF180": ["SF007", "LF090", "SB004", "LF090", "SB011"],
     "RB180": ["SF009", "RB180", "SB009"],
-    "LB180": ["SF010", "LB090", "SF002", "LB090", "SB008"],
-    # 270 degree turns
-    "RF270": ["SF007", "RF2700", "SB007"],
+    "LB180": ["SF009", "LB090", "SF002", "LB090", "SB009"],
+    # 270 degree turns: approximated using 180 degree turn + 90 degree turn
+    "RF270": ["SF008", "RF270", "SB008"], 
     "LF270": ["SF007", "LF090", "SB004", "LF090", "SB004", "LF090", "SB011"],
     "RB270": ["SF009", "RB270", "SB009"],
-    "LB270": ["SF010", "LB090", "SF002", "LB090", "SF002", "LB090", "SB008"]
+    "LB270": ["SF009", "LB090", "SF002", "LB180", "SB006"]
 }
 
 
