e8f698b68e52ad00e7da9b8b0638f82a1a41cc8f
nicolelst <nicolelim2608@gmail.com>
2023-10-18 20:41:37 +0800

    fix(rpi): update stm commands for moving with sensors and getting distances

--- rpi/mdp-rpi/rpi_config.py
+++ rpi/mdp-rpi/rpi_config.py
@@ -15,7 +15,7 @@ BT_BUFFER_SIZE = 2048
 STM_BAUDRATE = 115200
 STM_ACK_MSG = "A"
 # STM_NAV_COMMAND_FORMAT = '^[SLR][FB][0-9]{3}$' # task 1
-STM_NAV_COMMAND_FORMAT = '^(([SLR][FB]|OF)[0-9]{3})|([XY]DIST)$' # task 2
+STM_NAV_COMMAND_FORMAT = '^(([SLR][FB])|([UY]F)|([IX][LR]))[0-9]{3}$' # task 2
 STM_GYRO_RESET_COMMAND = "GYROR"
 STM_GYRO_RESET_DELAY = 8 # time to wait for gyro reset
 STM_GYRO_RESET_FREQ = 1414 # number of obstacles before GRYO RESET command is sent
@@ -63,8 +63,8 @@ STM_COMMAND_ADJUSTMENT_MAP = STM_COMMAND_ADJUSTMENT_DICT[LOCATION]
 STM_OBS_ROUTING_MAP = {
     "firstLeft": ["LF050", "RF050", "RF060", "LF040", "RB020"],
     "firstRight": ["RF050", "LF050", "LF060", "RF040", "LB020"],
-    "secondLeft": ["LF090", "OF065", "SB010", "RF180", "OF130", "RF090"],
-    "secondRight": ["RF090", "OF065", "SB010", "LF180", "OF130", "LF090"]
+    "secondLeft": ["LF090", "IR100", "RF180", "XR200", "RF090"],
+    "secondRight": ["RF090", "IL100", "LF180", "XL200", "RF090"]
 }
-STM_XDIST_COMMAND = "XDIST"
-STM_YDIST_COMMAND = "YDIST"
+STM_XDIST_COMMAND_FORMAT = "^[IX][LR][0-9]{3}$"
+STM_YDIST_COMMAND_FORMAT = "^[UY]F[0-9]{3}$"
